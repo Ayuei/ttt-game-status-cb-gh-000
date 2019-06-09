b23efcfcfb11e2b73189499051fe63a7df5cdda2
Vincent Nguyen <sefton_student@hotmail.com>
2019-06-09 04:27:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,11 +7,17 @@ end
 # 3 horizontal rows, 3 vertical columns, and 2 diagonals
 WIN_COMBINATIONS = [
   [0,1,2], # Top row
-  [3,4,5],  # Middle row
+  [3,4,5], # Middle row
   [6,7,8], # Bottom row
   [0,3,6], # 1st column
   [1,4,7], # 2nd column
   [2,5,8], # 3rd column
   [0,4,8], # Main axis Diagonal
-  [2,4,6] # Off axis diagonal
+  [2,4,6]  # Off axis diagonal
 ]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combination|
+    all_same = 
+  end
+end
